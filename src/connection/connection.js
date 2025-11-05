079decc4092eed88fb8ace91122861baed985397
Lubna Febin <115883767+lubnafebin@users.noreply.github.com>
2025-11-05 20:28:37 +0530

    Update connection.js

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const connect =async()=>{
-    await mongoose.connect('mongodb://localhost:27017/minnu', {
+    await mongoose.connect( process.env.MONGO_URI, {
         // useNewUrlParser: true,
         // useUnifiedTopology: true
       }).then(() => {
